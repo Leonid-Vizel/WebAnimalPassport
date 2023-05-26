37904d25a6c95c5024967c1def10806be2a2ba9f
Emil Maksudov <emil4ikmolor@gmail.com>
2023-05-26 22:12:09 +0300

    Хз чё

--- WebAnimalPassport/Data/ApplicationDbContext.cs
+++ WebAnimalPassport/Data/ApplicationDbContext.cs
@@ -1,17 +1,22 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebAnimalPassport.Models.Data;
 using WebAnimalPassport.Models.Data.Animal;
 using WebAnimalPassport.Models.Data.Treatment;
-using WebAnimalPassport.Models.Data.User;
 using WebAnimalPassport.Models.Data.Vaccination;
 
 namespace WebAnimalPassport.Data
 {
-    public partial class ApplicationDbContext : DbContext
+    public partial class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
         public DbSet<Treatment> Treatments { get; set; }
         public DbSet<Vaccination> Vaccinations { get; set; }
-        public DbSet<User> Users { get; set; }
+        public DbSet<CustomUser> Users { get; set; }
         public DbSet<Animal> Animals { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) => base.OnModelCreating(modelBuilder);
     }
 }
--- WebAnimalPassport/Program.cs
+++ WebAnimalPassport/Program.cs
@@ -1,7 +1,28 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebAnimalPassport.Data;
+using WebAnimalPassport.Models.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDefaultIdentity<CustomUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+builder.Services.AddRazorPages();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = $"/Identity/Accaunt/Login";
+    options.LogoutPath = $"/Identity/Accaunt/Logout";
+    options.AccessDeniedPath = $"/Identity/Accaunt/AccessDenied";
+});
 
 var app = builder.Build();
 
@@ -20,6 +41,7 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
