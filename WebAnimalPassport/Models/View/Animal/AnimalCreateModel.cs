8156366e8a001008655bf03c81f363d2257e5bac
Leonid-Vizel <spartanlaska@gmail.com>
2023-05-27 16:43:42 +0300

    [Animal-List,Index,Create] >Edit work continues

--- WebAnimalPassport/Models/View/Animal/AnimalCreateModel.cs
+++ WebAnimalPassport/Models/View/Animal/AnimalCreateModel.cs
@@ -5,8 +5,8 @@ namespace WebAnimalPassport.Models.View.Animal
 {
     public class AnimalCreateModel : AnimalBase
     {
-        [DisplayName("Фото питомца")]
-        public IFormFile File { get; set; }
+        [DisplayName("Фото питомца (необязательно)")]
+        public IFormFile? File { get; set; }
 
         public AnimalCreateModel() : base() { }
         public AnimalCreateModel(AnimalBase model) : base(model) { }
