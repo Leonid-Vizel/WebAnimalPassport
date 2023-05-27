b57068ef034decb6f1dbfda49f3a63fcf128b3f5
Emil Maksudov <emil4ikmolor@gmail.com>
2023-05-27 16:44:13 +0300

    Добавление возможности изменения необязательных данных в лично кабинете

--- WebAnimalPassport/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ WebAnimalPassport/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -2,13 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.ComponentModel.DataAnnotations;
-using System.Text.Encodings.Web;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using WebAnimalPassport.Models.Data;
 
 namespace WebAnimalPassport.Areas.Identity.Pages.Account.Manage
@@ -31,6 +29,12 @@ namespace WebAnimalPassport.Areas.Identity.Pages.Account.Manage
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
         public string Username { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string City { get; set; }
 
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -59,18 +63,51 @@ namespace WebAnimalPassport.Areas.Identity.Pages.Account.Manage
             [Phone]
             [Display(Name = "Phone number")]
             public string PhoneNumber { get; set; }
+
+            [DisplayName("Отчество")]
+            [MaxLength(1000, ErrorMessage = "Максимальная длина - 1000 символов!")]
+            public string Patronymic { get; set; }
+
+            [DisplayName("Регион")]
+            [MaxLength(1000, ErrorMessage = "Максимальная длина - 1000 символов!")]
+            public string Region { get; set; }
+
+            [DisplayName("Страна")]
+            [MaxLength(1000, ErrorMessage = "Максимальная длина - 1000 символов!")]
+            public string Country { get; set; }
+
+            [DisplayName("Адрес")]
+            [MaxLength(1000, ErrorMessage = "Максимальная длина - 1000 символов!")]
+            public string Address { get; set; }
+
+            [DisplayName("Индекс")]
+            [MaxLength(1000, ErrorMessage = "Максимальная длина - 1000 символов!")]
+            public string Index { get; set; }
         }
 
         private async Task LoadAsync(CustomUser user)
         {
             var userName = await _userManager.GetUserNameAsync(user);
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            var patronymic = user.Patronymic;
+            var region = user.Region;
+            var country = user.Country;
+            var address = user.Address;
+            var index = user.Index;
 
             Username = userName;
+            Name = user.Name;
+            Surname = user.Surname;
+            City = user.City;
 
             Input = new InputModel
             {
-                PhoneNumber = phoneNumber
+                PhoneNumber = phoneNumber,
+                Patronymic = patronymic,
+                Address = address,
+                Index = index,
+                Country = country,
+                Region = region
             };
         }
 
@@ -109,6 +146,37 @@ namespace WebAnimalPassport.Areas.Identity.Pages.Account.Manage
                     StatusMessage = "Unexpected error when trying to set phone number.";
                     return RedirectToPage();
                 }
+            }
+
+            var patronymic = user.Patronymic;
+            var region = user.Region;
+            var country = user.Country;
+            var address = user.Address;
+            var index = user.Index;
+            if (Input.Patronymic != patronymic)
+            {
+                user.Patronymic = Input.Patronymic;
+                await _userManager.UpdateAsync(user);
+            }
+            if (Input.Country != country)
+            {
+                user.Country = Input.Country;
+                await _userManager.UpdateAsync(user);
+            }
+            if (Input.Address != address)
+            {
+                user.Address = Input.Address;
+                await _userManager.UpdateAsync(user);
+            }
+            if (Input.Index != index)
+            {
+                user.Index = Input.Index;
+                await _userManager.UpdateAsync(user);
+            }
+            if (Input.Region != region)
+            {
+                user.Region = Input.Region;
+                await _userManager.UpdateAsync(user);
             }
 
             await _signInManager.RefreshSignInAsync(user);
